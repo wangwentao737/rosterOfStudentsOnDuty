d9644185fe7f02e034a409279560eea6bad4f749
wwt <wangwentao0119@outlook.com>
2024-09-25 00:34:23 +0800

    basci func1

--- index.js
+++
@@ -1,4 +0,0 @@
-const container = document.querySelector('.container'); 
-const swept = document.querySelector('.swept');
-const blackboard = document.querySelector('.blackboard');
-const data = new Date().getDay();
---
+++ js/index.js
@@ -0,0 +1,55 @@
+const students = [{
+    swept: 1,
+    blackboard: 2,
+},{
+    swept: 2,
+    blackboard: 1,
+},{
+    swept: 3,
+    blackboard: 4,
+},{
+    swept: 4,
+    blackboard: 3,
+},
+]
+const container = document.querySelector('.container'); 
+const swept = document.querySelector('.swept');
+const blackboard = document.querySelector('.blackboard');
+function studentChange(counts) {
+    swept.innerHTML = students[counts].swept;
+    blackboard.innerHTML = students[counts].blackboard;
+}
+function changeOutData(data,num) {
+    
+}
+function countChange(num) {
+    const maxCount = students.length;
+    if (num == 1) {
+        if (counts== maxCount) {
+            counts = 0;
+            changeOutData(counts,2)
+        } else {
+            counts == counts+1;
+            changeOutData(counts,2)
+        }
+    }
+    if (num == 0) {
+        if (counts == 0) {
+            counts = maxCount;
+            changeOutData(counts,2)
+        }else {
+            counts == counts-1;
+            changeOutData(counts,2)
+        }
+    }
+}
+function nextGroup() {
+    countChange(1)
+    studentChange()
+}
+function backGroup() {
+    countChange(0)
+    studentChange()
+}
+
+
