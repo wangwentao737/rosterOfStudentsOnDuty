8293bd3f0934ea865e85f93f341c83d48ace9258
wwt <wangwentao0119@outlook.com>
2024-09-29 00:13:15 +0800

    每日轮换+快捷键修改

--- js/index.js
+++ js/index.js
@@ -1,52 +1,116 @@
 const fs = require('fs');
 const path = require('path');
-const data = fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8');
-const jsonData = JSON.parse(data);
-
-
-const container = document.querySelector('.container'); 
 const swept = document.querySelector('.swept');
 const blackboard = document.querySelector('.blackboard');
+
 function studentChange(counts) {
     swept.innerHTML = students[counts].swept;
     blackboard.innerHTML = students[counts].blackboard;
 }
-function changeOutData(data,num) {
-    
+
+function getCountNumber() {
+    const data = fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8');
+    const jsonData = JSON.parse(data);
+    return +jsonData.array[2];
 }
+
+/**
+ * @param num {number} 0为月份 1为日期
+ */
+function getDataNumber(num) {
+    const data = fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8');
+    const jsonData = JSON.parse(data);
+    if (num == 0) {
+        return +jsonData.array[0];
+    } else if (num == 1) {
+        return +jsonData.array[1];
+    } 
+}
+    
+/**
+ *@param num {number} 注意0为第一组
+*/
+function wirteCountNumber(num) {
+    const data = fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8');
+    const jsonData = JSON.parse(data);
+    jsonData.array[2] = num;
+    fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(jsonData));
+}
+
+/**
+ * @param data {number} 更改的数字 
+ * @param num {number} 0为月份 1为日期
+*/
+function wirteOuterData(datas,num) {
+    const data = fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8');
+    const jsonData = JSON.parse(data);
+    jsonData.array[num] = datas;
+    fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(jsonData));
+}
+
+/**
+ *@param num {number} 1为进 0为退
+*/
 function countChange(num) {
     const maxCount = students.length - 1;
+    let counts = getCountNumber();
     if (num == 1) {
         if (counts== maxCount) {
             counts = 0;
-            changeOutData(counts,2)
+            wirteCountNumber(counts)
         } else {
-            counts == counts+1;
-            changeOutData(counts,2)
+            counts = counts + 1;
+            wirteCountNumber(counts)
         }
     }
     if (num == 0) {
         if (counts == 0) {
             counts = maxCount;
-            changeOutData(counts,2)
+            wirteCountNumber(counts)
         }else {
-            counts == counts-1;
-            changeOutData(counts,2)
+            counts = counts - 1;
+            wirteCountNumber(counts)
         }
     }
 }
+
+function dailyChangeOuterData() {
+    const month = new Date().getMonth()+1;
+    const day = new Date().getDate();
+    if (month !== getDataNumber(0) || day !== getDataNumber(1)) {
+        wirteOuterData(month,0);
+        wirteOuterData(day,1);
+        countChange(1)
+        studentChange(getCountNumber())
+    } else {
+        studentChange(getCountNumber())
+    }
+}
+
 function nextGroup() {
     countChange(1)
-    studentChange()
+    studentChange(getCountNumber())
 }
+
 function backGroup() {
     countChange(0)
-    studentChange()
+    studentChange(getCountNumber())
 }
 
-// swept.innerHTML = jsonData.array
-swept.innerHTML = students[0].swept;    
+window.addEventListener('keydown', (e) => {
+    if (e.key == 'ArrowRight'&& e.ctrlKey) {
+        nextGroup()
+    }
+    if (e.key == 'ArrowLeft' && e.ctrlKey) {
+        backGroup()
+    }
+    if (e.key == 'T' && e.ctrlKey) {
+        
+        nextGroup()
+    }
+})
 
+window.addEventListener('load', dailyChangeOuterData)
 
 
 
